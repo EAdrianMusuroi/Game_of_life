7a624c8da864f72007e87edfa38d3da729430744
EAdrianMusuroi <adi.musuroi95@gmail.com>
2020-01-26 02:53:42 +0200

    Simple implementation benchmark

---
+++ scripts/run_GOL.sh
@@ -0,0 +1,26 @@
+# Run all configurations for simple implementation
+
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 50 50 25 /home/adrian/Documents/PA/Game_of_life/input/input_size_50
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 50 50 100 /home/adrian/Documents/PA/Game_of_life/input/input_size_50
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 50 50 500 /home/adrian/Documents/PA/Game_of_life/input/input_size_50
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 50 50 1000 /home/adrian/Documents/PA/Game_of_life/input/input_size_50
+
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 150 150 25 /home/adrian/Documents/PA/Game_of_life/input/input_size_150
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 150 150 100 /home/adrian/Documents/PA/Game_of_life/input/input_size_150
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 150 150 500 /home/adrian/Documents/PA/Game_of_life/input/input_size_150
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 150 150 1000 /home/adrian/Documents/PA/Game_of_life/input/input_size_150
+
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 500 500 25 /home/adrian/Documents/PA/Game_of_life/input/input_size_500
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 500 500 100 /home/adrian/Documents/PA/Game_of_life/input/input_size_500
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 500 500 500 /home/adrian/Documents/PA/Game_of_life/input/input_size_500
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 500 500 1000 /home/adrian/Documents/PA/Game_of_life/input/input_size_500
+
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1000 1000 25 /home/adrian/Documents/PA/Game_of_life/input/input_size_1000
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1000 1000 100 /home/adrian/Documents/PA/Game_of_life/input/input_size_1000
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1000 1000 500 /home/adrian/Documents/PA/Game_of_life/input/input_size_1000
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1000 1000 1000 /home/adrian/Documents/PA/Game_of_life/input/input_size_1000
+
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1500 1500 25 /home/adrian/Documents/PA/Game_of_life/input/input_size_1500
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1500 1500 100 /home/adrian/Documents/PA/Game_of_life/input/input_size_1500
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1500 1500 500 /home/adrian/Documents/PA/Game_of_life/input/input_size_1500
+/home/adrian/Documents/PA/Game_of_life/game_of_life/game_of_life 1500 1500 1000 /home/adrian/Documents/PA/Game_of_life/input/input_size_1500
